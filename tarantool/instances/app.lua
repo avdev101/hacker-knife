fa31a60a82a3cbdca0793e9a4869c2aa99b8a663
aleksey <eremeev.crypt@gmail.com>
2021-12-07 00:13:52 +0300

    create domain/subdomain schema

---
+++ tarantool/instances/app.lua
@@ -0,0 +1,39 @@
+box.cfg{
+    background = false,
+    listen = '0.0.0.0:3722',
+    log = '| tee',
+    log_format = 'plain',
+}
+
+local function schema_v1()
+    local domain = box.schema.space.create('domain')
+
+    domain:format({
+        {name='name', type='string'}
+    })
+    domain:create_index('primary', {
+        type='hash',
+        parts={'name'}
+    })
+
+    local subdomain = box.schema.space.create('subdomain')
+
+    subdomain:format({
+        {name='domain', type='string'},
+        {name='name', type='string'},
+        {name='cname', type='string'},
+        {name='is_new', type='boolean'},
+    })
+    subdomain:create_index('primary', {
+        type='hash',
+        parts={'name'}
+    })
+    subdomain:create_index('domain', {
+        type='tree',
+        parts={'domain'},
+        unique=false
+    })
+
+end
+
+box.once('schema_v1', schema_v1)
