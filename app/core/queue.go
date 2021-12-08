4ea6d2f3fea9e99118dc7dbdb3e3bc1109bf94bb
aleksey <eremeev.crypt@gmail.com>
2021-12-08 20:36:12 +0300

    add queue events struct

--- app/core/queue.go
+++ app/core/queue.go
@@ -1,5 +1,27 @@
 package core
 
+type ParseSubDomainTask struct {
+	DomainName string
+}
+
+type ParseIpTask struct {
+	DomainName string
+}
+
+type ParsePortTask struct {
+	IP string
+}
+
+type FindServiceTask struct {
+	IP string
+}
+
+type FindPathTask struct {
+	DomainName string
+	PortNumber int
+	IsHttps    bool
+}
+
 type DomainQueue interface {
 	PutSubdomainEnumerate(d Domain) error
 	TakeSubdomainEnumerate() (Domain, error)
