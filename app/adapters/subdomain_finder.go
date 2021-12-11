6b62375c35d0806721e0faa5f2737b580e880c59
aleksey <eremeev.crypt@gmail.com>
2021-12-12 00:29:01 +0300

    create dummy subdomain finder

---
+++ app/adapters/subdomain_finder.go
@@ -0,0 +1,14 @@
+package adapters
+
+import "github.com/eremeevdev/hacker-knife/core"
+
+type DummySubdomainFinder struct {
+}
+
+func (f *DummySubdomainFinder) Enumerate(domain string) ([]core.SubdomainFindItem, error) {
+	result := make([]core.SubdomainFindItem, 2)
+	result = append(result, core.SubdomainFindItem{"ya.ru", "x.ya.ru", "xx"})
+	result = append(result, core.SubdomainFindItem{"ya.ru", "y.ya.ru", "yy"})
+
+	return result, nil
+}
