2cf246b8bdc2f172dc32cc6e3cd6f1a96b0cad32
aleksey <eremeev.crypt@gmail.com>
2021-12-12 01:08:28 +0300

    fix

--- app/adapters/subdomain_finder.go
+++ app/adapters/subdomain_finder.go
@@ -6,7 +6,7 @@ type DummySubdomainFinder struct {
 }
 
 func (f *DummySubdomainFinder) Enumerate(domain string) ([]core.SubdomainFindItem, error) {
-	result := make([]core.SubdomainFindItem, 2)
+	result := make([]core.SubdomainFindItem, 0)
 	result = append(result, core.SubdomainFindItem{"ya.ru", "x.ya.ru", "xx"})
 	result = append(result, core.SubdomainFindItem{"ya.ru", "y.ya.ru", "yy"})
 
