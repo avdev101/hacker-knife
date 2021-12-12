c8a85be056276c321fd3f7abcaba2c8dd93819b6
aleksey <eremeev.crypt@gmail.com>
2021-12-12 16:05:22 +0300

    update domain consumer

--- app/cmd/domain_consumer/main.go
+++ app/cmd/domain_consumer/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/eremeevdev/hacker-knife/adapters"
 	"github.com/eremeevdev/hacker-knife/adapters/tarantool"
+	"github.com/eremeevdev/hacker-knife/core"
 )
 
 func main() {
@@ -12,11 +15,30 @@ func main() {
 		panic(err)
 	}
 
-	task, err := q.TakeSubdomain()
+	r, err := tarantool.NewSubdomainRepo("tarantool_app:3722", "admin", "pass")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(task)
-	task.Ack()
+	f := adapters.DummySubdomainFinder{}
+
+	s := core.DomainEnumerateService{&r, &f, &q}
+
+	for {
+		task, err := q.TakeSubdomain()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(task)
+
+		err = s.Enumerate(task.Data.DomainName, task.Data.Propagate)
+		if err != nil {
+			log.Fatal(err)
+			task.Nack()
+		} else {
+			task.Ack()
+		}
+	}
+
 }
