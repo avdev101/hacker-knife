61c9da13521f47f2333d2efd49b866701c395f7b
aleksey <eremeev.crypt@gmail.com>
2021-12-13 01:40:08 +0300

    implement ip collect service

--- app/core/ip_collect.go
+++ app/core/ip_collect.go
@@ -7,6 +7,47 @@ type IPCollectService struct {
 	IPCollector IPCollector
 }
 
-func (s *IPCollectService) Collect(domain string) error {
+func (s *IPCollectService) Collect(domain string, stopPropagate bool) error {
+
+	existing, err := s.IPRepo.GetBySubdomain(domain)
+	if err != nil {
+		return err
+	}
+
+	collected, err := s.IPCollector.Collect(domain)
+	if err != nil {
+		return err
+	}
+
+	err = s.saveDiff(existing, collected)
+	if err != nil {
+		return err
+	}
+
+	if !stopPropagate {
+		s.propagateTask(collected)
+	}
+
+	return nil
+}
+
+func (s *IPCollectService) saveDiff(existing []IP, collected []IPCollectItem) error {
+
+	diff := IPDiff{existing, collected}
+
+	s.IPRepo.CreateBatch(diff.getNew())
+	s.IPRepo.DeleteBatch(diff.getDeleted())
+
+	return nil
+}
+
+func (s *IPCollectService) propagateTask(collected []IPCollectItem) error {
+
+	for _, ip := range collected {
+		t := CollectServiceCommand{IP: ip.Addr}
+		s.TaskQeue.CollectService(t)
+
+	}
+
 	return nil
 }
