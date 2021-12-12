5ea356de86fa73274bc704022a35b7896e9e56c3
aleksey <eremeev.crypt@gmail.com>
2021-12-12 11:51:10 +0300

    implement subdomain get list

--- app/adapters/tarantool/subdomain_repo.go
+++ app/adapters/tarantool/subdomain_repo.go
@@ -1,13 +1,57 @@
 package tarantool
 
-import "github.com/eremeevdev/hacker-knife/core"
+import (
+	"fmt"
+
+	"github.com/eremeevdev/hacker-knife/core"
+	"github.com/tarantool/go-tarantool"
+)
 
 type SubdomainRepo struct {
+	conn *tarantool.Connection
+}
+
+func NewSubdomainRepo(host string, user string, pass string) (SubdomainRepo, error) {
+	conn, err := tarantool.Connect(host, tarantool.Opts{
+		User: user,
+		Pass: pass,
+	})
+
+	if err != nil {
+		return SubdomainRepo{}, fmt.Errorf("can't create connection: %v", err)
+	}
+
+	return SubdomainRepo{conn}, nil
+}
+
+func tuplesToSubdomains(tuples [][]interface{}) []core.Subdomain {
+	result := make([]core.Subdomain, 0)
+
+	for _, tuple := range tuples {
+		item := core.Subdomain{
+			Domain: tuple[0].(string),
+			Name:   tuple[1].(string),
+			Cname:  tuple[2].(string),
+			IsNew:  tuple[3].(bool),
+		}
+		result = append(result, item)
+	}
+
+	return result
 }
 
 func (r *SubdomainRepo) GetList(domainName string) ([]core.Subdomain, error) {
-	result := make([]core.Subdomain, 0)
-	return result, nil
+
+	resp, err := r.conn.Select("subdomain", "domain", 0, 10000, tarantool.IterEq, []interface{}{domainName})
+	if err != nil {
+		return nil, err
+	}
+
+	tuples := resp.Tuples()
+
+	subdomains := tuplesToSubdomains(tuples)
+
+	return subdomains, nil
 }
 
 func (r *SubdomainRepo) DeleteBatch(domains []core.Subdomain) error {
