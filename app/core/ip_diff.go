6751c6993f538bb9fc0d59c59871980886636684
aleksey <eremeev.crypt@gmail.com>
2021-12-15 23:13:16 +0300

    implement ip diff

--- app/core/ip_diff.go
+++ app/core/ip_diff.go
@@ -5,10 +5,62 @@ type IPDiff struct {
 	found    []IPCollectItem
 }
 
+func (d *IPDiff) getExistingMap() map[string]IP {
+	result := make(map[string]IP)
+
+	for _, ip := range d.existing {
+		result[ip.Addr] = ip
+	}
+
+	return result
+}
+
+func (d *IPDiff) makeNewIP(foundIP IPCollectItem) IP {
+	return IP{
+		Domain: foundIP.Domain,
+		Addr:   foundIP.Addr,
+		IsNew:  true,
+	}
+}
+
 func (d *IPDiff) getNew() []IP {
-	return nil
+	existingMap := d.getExistingMap()
+
+	result := make([]IP, 0)
+
+	for _, foundIP := range d.found {
+		_, ok := existingMap[foundIP.Addr]
+
+		if !ok {
+			result = append(result, d.makeNewIP(foundIP))
+		}
+	}
+
+	return result
+}
+
+func (d IPDiff) getCollectedMap() map[string]IPCollectItem {
+	result := make(map[string]IPCollectItem)
+
+	for _, ip := range d.found {
+		result[ip.Addr] = ip
+	}
+
+	return result
 }
 
 func (d *IPDiff) getDeleted() []IP {
-	return nil
+	result := make([]IP, 0)
+
+	collectedMap := d.getCollectedMap()
+
+	for _, ip := range d.existing {
+		_, ok := collectedMap[ip.Addr]
+
+		if !ok {
+			result = append(result, ip)
+		}
+	}
+
+	return result
 }
