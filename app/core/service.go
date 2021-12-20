3ae7fb86ea429e441a6df091936fc7d6c2148cd1
aleksey <eremeev.crypt@gmail.com>
2021-12-20 20:46:30 +0300

    update Service type fields

--- app/core/service.go
+++ app/core/service.go
@@ -1,12 +1,11 @@
 package core
 
 type Service struct {
-	ParentDomain string
-	Domain       string
-	IPAddr       string
-	PortNumber   int
-	ServiceType  string
-	IsNew        bool
+	Domain      string
+	IPAddr      string
+	PortNumber  int
+	ServiceType string
+	IsNew       bool
 }
 
 type ServiceRepo interface {
